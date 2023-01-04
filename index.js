312916e0d395d4761b3780b99aa31ce220c87504
rajeshwar singh bisht <ynegi6@gmail.com>
2023-01-04 23:40:49 +0000

    CounterStr added to track passenger count

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let countEl = document.getElementById("count-el")
+let saveEL = document.getElementById("save-el")
 
 
 let count = 0
@@ -8,9 +9,18 @@ function increment() {
   
 }
 
-
 function save() {
+    let countStr = count + " - "
+    console.log(incremtal)
+    saveEL.innerText += countStr
     console.log(count)
 }
 
-save()
+// let name = "Yogita"
+// let greeting = "Welcome back "
+// welcomeEL.innerText =  greeting + name
+
+// welcomeEL.innerText += "🫶"
+
+let incremtal = count + " "+ "-"
+    console.log(incremtal)
