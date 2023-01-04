aceb469cb94b46dc3376b133332f921b2238b66b
rajeshwar singh bisht <ynegi6@gmail.com>
2023-01-04 02:51:56 +0000

    Add count to count-el id

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+let countEl = document.getElementById("count-el")
+
 let count = 0
 function increment() {
     count += 1
-    console.log(count)
+    countEl.innerText = count
+  
 }
 
-increment()
 
